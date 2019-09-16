d45a21cdfbe259d6d0b04367c4a569ab7a3e7ddc
qris <qris09@gmail.com>
2019-09-16 21:34:32 +0100

    saved tasks

---
+++ app/src/main/java/com/example/qristask1/Register.java
@@ -0,0 +1,43 @@
+package com.example.qristask1;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register);
+
+        final EditText userName = (EditText) findViewById(R.id.NewName);
+        final EditText password = (EditText) findViewById(R.id.NewPassword);
+        final EditText email = (EditText) findViewById(R.id.NewEmail);
+        Button btnRegister = (Button) findViewById(R.id.btnNewRegister);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                SharedPreferences preferences = getSharedPreferences("MYPREFS", MODE_PRIVATE);
+                String newUser = userName.getText().toString();
+                String newPassword = password.getText().toString();
+                String newEmail = email.getText().toString();
+
+                SharedPreferences.Editor editor = preferences.edit();
+
+                editor.putString(newUser + newPassword + "data", newUser + "\n" + newEmail);
+                editor.commit();
+
+                Intent loginScreen = new Intent(Register.this, MainActivity.class);
+                startActivity(loginScreen);
+            }
+        });
+    }
+}
